be19bfb2f1b5b8894ab538a6ad733d71aaa6a4f1
kang <1243936032@qq.com>
2019-07-23 22:57:10 +0800

    add Vant Weapp

--- my-project/src/pages/index/index.vue
+++ my-project/src/pages/index/index.vue
@@ -15,14 +15,11 @@
         <card :text="motto"></card>
       </div>
     </div>
-
-    <form class="form-container">
-      <input type="text" class="form-control" :value="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-
-
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
     <div class="all">
         <div class="left">
         </div>
